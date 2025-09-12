0378037fcb4461cab903f3242c96c896ecd39e37
abirneu <abirhasan.shu@gmail.com>
2025-09-13 01:35:07 +0600

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ import TopProducts from './components/TopProducts/TopProducts';
 import Banner from './components/Banner/Banner';
 import Subscribe from './components/Subscribe/Subscribe';
 import Testimonials from './components/Testimonials/Testimonials';
+import Popup from './components/Popup/Popup';
 import Footer from './components/Footer/Footer';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // Import AOS styles
 
 const App = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  }
+
+
   React.useEffect(() => {
     AOS.init({
       offset: 100, // Offset (in pixels) from the original trigger point
@@ -23,14 +31,15 @@ const App = () => {
 
   return (
     <div >
-      <Navbar/>
-      <Hero/>
+      <Navbar handleOrderPopup={handleOrderPopup}/>
+      <Hero handleOrderPopup={handleOrderPopup}/>
       <Products/>
-      <TopProducts/>
+      <TopProducts handleOrderPopup={handleOrderPopup}/>
       <Banner/>
       <Subscribe/>
       <Testimonials/>
       <Footer/>
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
 };
